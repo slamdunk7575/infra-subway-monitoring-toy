956b58860ed1a8e44745b05b07c2f206042ba414
slamdunk7575 <slamdunk7575@gmail.com>
2021-06-28 03:06:24 +0900

    (refactor) assertThat 임포트

--- src/test/java/nextstep/subway/auth/application/AuthServiceTest.java
+++ src/test/java/nextstep/subway/auth/application/AuthServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
